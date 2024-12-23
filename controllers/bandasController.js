85ae419d6184afa784e74091e05fb63b38b7e628
Jona(unDEV) <jonatanemanuelsilva6@gmail.com>
2024-12-22 23:09:24 -0300

    add css y modificaciones en las arrows functions

--- controllers/bandasController.js
+++ controllers/bandasController.js
@@ -4,22 +4,17 @@ const bandasController = {
   listadoBandas: (req, res) => {
     res.render('listadoBandas', { bandas: db.lista });
   },
+
   detalleBanda: (req, res) => {
     const banda = db.lista.find(b => b.id == req.params.id);
-    if (banda) {
-      res.render('detalleBanda', { banda });
-    } else {
-      res.send('Banda no encontrada');
-    }
+    res.render('detalleBanda', { banda });
   },
+
   porGenero: (req, res) => {
     const bandas = db.lista.filter(b => b.genero.toLowerCase() == req.params.genero.toLowerCase());
-    if (bandas.length > 0) {
-      res.render('porGenero', { bandas });
-    } else {
-      res.send('No hay bandas de ese género');
-    }
-  }
+    res.render('porGenero', { bandas });
+  },
+  
 };
 
 module.exports = bandasController;
